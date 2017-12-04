33929a1664c86ab3fb12e8f8a7e5d8fad14501aa
lukeclee <33207332+lukeclee@users.noreply.github.com>
2017-12-03 18:36:42 -0800

    Update game.h

--- game.h
+++ game.h
@@ -1,20 +1,32 @@
+#pragma once
+#include "stdafx.h"
+#include "players.h"
+
+class Player;
+
+enum GameState {TurnP1, TurnP2, Draw, P1Won, P2Won};
+enum BoardField {Empty, Player1, Player2};
 
 class Game
 {
 public:
 
-	const int BoardWidth;
-	const int BoardHeight;
+	const int BoardWidth = 7;
+	const int BoardHeight = 6;
 
 	Game(Player &p1, Player &p2);
 
-	void getState();
+	GameState getState();
 
 	bool isRunning();
 
-	int operator() (int x, int y) const;
+	BoardField operator() (int x, int y) const;
 
 	void nextTurn();
 
-
+private:
+	GameState currentState;
+	BoardField** board;
+	Player* player1;
+	Player* player2;
 };
